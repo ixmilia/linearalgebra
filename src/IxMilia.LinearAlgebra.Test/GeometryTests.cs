5011de875453166c3071b24a5001eadd2671a3dd
Brett V. Forsgren <brett.forsgren@outlook.com>
2019-11-26 12:17:18 -0800

    add `Vector1` abstraction

--- src/IxMilia.LinearAlgebra.Test/GeometryTests.cs
+++ src/IxMilia.LinearAlgebra.Test/GeometryTests.cs
@@ -6,6 +6,38 @@ namespace IxMilia.LinearAlgebra.Test
     public class GeometryTests : TestBase
     {
         [Fact]
+        public void Vector1SimpleScaleTest()
+        {
+            var vector = new Vector1(1.0);
+            var translate = Vector1.CreateScale(5.0);
+            var result = translate * vector;
+            var expected = new Vector1(5.0);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Vector1SimpleTranslateTest()
+        {
+            var vector = new Vector1(1.0);
+            var translate = Vector1.CreateTranslate(5.0);
+            var result = translate * vector;
+            var expected = new Vector1(6.0);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(32.0, 0.0)]
+        [InlineData(212.0, 100.0)]
+        public void Vector1CombineTransformationsTest(double input, double output)
+        {
+            // converting F to C
+            var transform = Vector1.CreateScale(5.0 / 9.0) * Vector1.CreateTranslate(-32.0);
+            var result = transform * new Vector1(input);
+            Assert.Equal(1.0, result[1, 0]);
+            Assert.Equal(output, result[0, 0]);
+        }
+
+        [Fact]
         public void Vector2SimpleScaleTest()
         {
             var vector = new Vector2(1.0, 2.0);
---
+++ src/IxMilia.LinearAlgebra/Geometry/Vector1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace IxMilia.LinearAlgebra.Geometry
+{
+    public class Vector1 : Matrix
+    {
+        public double X
+        {
+            get { return this[0, 0]; }
+            set { this[0, 0] = value; }
+        }
+
+        public double W
+        {
+            get { return this[1, 0]; }
+            set { this[1, 0] = value; }
+        }
+
+        public double LengthSquared => X * X;
+
+        public double Length => Math.Sqrt(LengthSquared);
+
+        public bool IsInfinity => W == 0.0;
+
+        private Vector1()
+            : base(2, 1)
+        {
+        }
+
+        public Vector1(double x)
+            : this(x, 1.0)
+        {
+        }
+
+        public Vector1(double x, double w)
+            : this()
+        {
+            X = x;
+            W = w;
+        }
+
+        public void TryNormalize()
+        {
+            if (W == 0.0)
+            {
+                return;
+            }
+
+            X /= W;
+            W = 1.0;
+        }
+
+        public override string ToString()
+        {
+            return $"({X})";
+        }
+
+        public static Vector1 Infinity => new Vector1(0, 0); // TODO: should this be 0, 1?
+
+        public static Vector1 ZeroVector => new Vector1(0.0);
+
+        public static Matrix CreateScale(double sx)
+        {
+            return new Matrix(2, 2,
+                sx, 0.0,
+                0.0, 1.0);
+        }
+
+        public static Matrix CreateTranslate(double dx)
+        {
+            return new Matrix(2, 2,
+                1.0, dx,
+                0.0, 1.0);
+        }
+    }
+}
